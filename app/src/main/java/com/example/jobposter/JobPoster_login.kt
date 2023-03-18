1fc4751fbb9356cad8c42bcedde50a40b1b94cac
Veynitha Bandara <veynithabandara@gmail.com>
2023-03-18 08:33:39 +0530

    first Second Commit

---
+++ app/src/main/java/com/example/jobposter/JobPoster_login.kt
@@ -0,0 +1,11 @@
+package com.example.jobposter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JobPoster_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_poster_login)
+    }
+}
