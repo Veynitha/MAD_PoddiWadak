ed87b74c1f8a3de93d7c1f4e764dc4b4a0e2304b
Veynitha Bandara <veynithabandara@gmail.com>
2023-04-24 18:54:06 +0530

    Navigation implemented

--- app/src/main/java/com/example/jobposter/Job_Poster_register.kt
+++ app/src/main/java/com/example/jobposter/Job_Poster_register.kt
@@ -1,11 +1,40 @@
 package com.example.jobposter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Job_Poster_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_poster_register)
+
+        val homeButton = findViewById<Button>(R.id.btn_Home)
+        val notifyButton = findViewById<Button>(R.id.btn_Notification)
+        val jobsButton = findViewById<Button>(R.id.btn_Jobs)
+        val profileButton = findViewById<Button>(R.id.btn_Profile)
+        val feedbackButton = findViewById<Button>(R.id.btn_More)
+
+        homeButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        notifyButton.setOnClickListener{
+            val intent = Intent(this, Job_Poster_register::class.java)
+            startActivity(intent)
+        }
+        jobsButton.setOnClickListener{
+            val intent = Intent(this, Job_page::class.java)
+            startActivity(intent)
+        }
+        profileButton.setOnClickListener{
+            val intent = Intent(this, JobPoster_profile::class.java)
+            startActivity(intent)
+        }
+        feedbackButton.setOnClickListener{
+            val intent = Intent(this, Insert_Job::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/jobposter/Job_page.kt
+++ app/src/main/java/com/example/jobposter/Job_page.kt
@@ -1,11 +1,14 @@
 package com.example.jobposter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Job_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_page)
+
     }
 }
