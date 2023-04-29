06f7bafa2a6ca2991ebe017062e699bbe10bc36e
Veynitha Bandara <veynithabandara@gmail.com>
2023-04-29 16:01:13 +0530

    Job Poster-Register and Login: Complete

--- app/src/main/java/com/example/jobposter/JobPoster_login.kt
+++ app/src/main/java/com/example/jobposter/JobPoster_login.kt
@@ -1,41 +1,132 @@
 package com.example.jobposter
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.Toast
+import com.example.jobposter.databinding.ActivityJobPosterLoginBinding
+import com.example.jobposter.databinding.ActivityJobPosterRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class JobPoster_login : AppCompatActivity() {
+    //Hello World
+
+    //view binding
+    private lateinit var binding: ActivityJobPosterLoginBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //progress dialog
+    //private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_job_poster_login)
+        binding = ActivityJobPosterLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val homeButton = findViewById<Button>(R.id.btn_Home)
-        val notifyButton = findViewById<Button>(R.id.btn_Notification)
-        val jobsButton = findViewById<Button>(R.id.btn_Jobs)
-        val profileButton = findViewById<Button>(R.id.btn_Profile)
-        val feedbackButton = findViewById<Button>(R.id.btn_More)
+       //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        homeButton.setOnClickListener{
+        binding.btnLogin.setOnClickListener{
+            validateData()
+        }
+
+
+
+
+        //navigate to register job_poster
+        binding.tvCreateAccount.setOnClickListener{
+            startActivity(Intent(this, Job_Poster_register::class.java))
+        }
+
+        //-----------nav bar implementation-------------------//
+        binding.btnHome.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        notifyButton.setOnClickListener{
+        binding.btnNotification.setOnClickListener{
             val intent = Intent(this, Job_Poster_register::class.java)
             startActivity(intent)
         }
-        jobsButton.setOnClickListener{
+        binding.btnJobs.setOnClickListener{
             val intent = Intent(this, Job_page::class.java)
             startActivity(intent)
         }
-        profileButton.setOnClickListener{
+        binding.btnProfile.setOnClickListener{
             val intent = Intent(this, JobPoster_profile::class.java)
             startActivity(intent)
         }
-        feedbackButton.setOnClickListener{
+        binding.btnMore.setOnClickListener{
             val intent = Intent(this, Insert_Job::class.java)
             startActivity(intent)
         }
+    }
+    //initializing variables to store data
+    private var email=""
+    private var password=""
 
+    private fun validateData() {
+        //getting data from login form
+        email = binding.etFullName.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+
+        //Validating data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show()
+        }
+        else if (password.isEmpty()){
+            Toast.makeText(this, "Enter a password to proceed", Toast.LENGTH_SHORT).show()
+        }else{
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        //login using firebase auth
+
+       // progressDialog.setMessage("Logging in...")
+        //progressDialog.show()
+
+        //checking user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener{
+                //progressDialog.setMessage("Logged in")
+                startActivity(Intent(this@JobPoster_login, JobPoster_profile::class.java))
+                finish()
+            }
+            .addOnFailureListener {e->
+                //progressDialog.dismiss()
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@JobPoster_login, MainActivity::class.java))
+            }
+    }
+
+    private fun checkUser() {
+        //checking user and the user type
+
+        //progressDialog.setMessage("Checking User...")
+
+        val firebaseUser = firebaseAuth.currentUser!!
+        val ref = FirebaseDatabase.getInstance("https://podiwadak-a4d20-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
+        ref.child(firebaseUser.uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                   //progressDialog.setMessage("Logged in")
+                   startActivity(Intent(this@JobPoster_login, JobPoster_profile::class.java))
+                    finish()
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    //progressDialog.dismiss()
+
+                }
+            })
     }
 }
